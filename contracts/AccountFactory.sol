9b53020ff2c7639bfa250c55dd7608bb00000fe0
Valeriy Loshchenkov <codenamenoon@gmail.com>
2024-04-15 20:33:48 +0300

    WIP

--- contracts/AccountFactory.sol
+++ contracts/AccountFactory.sol
@@ -4,13 +4,13 @@ pragma solidity 0.8.24;
 import { ProxyAccount } from "./ProxyAccount.sol";
 
 contract AccountFactory {
-    uint256 public counter;
+    mapping(address user => uint256 salt) public counter;
 
     event CreateAccount(address owner, uint256 salt, address account);
 
     function createAccount(address owner) public {
-        address account = address(new ProxyAccount{salt: bytes32(counter++)}(owner));
+        address account = address(new ProxyAccount{salt: bytes32(counter[owner])}(owner));
 
-        emit CreateAccount(owner, counter, account);
+        emit CreateAccount(owner, counter[owner]++, account);
     }
 }
